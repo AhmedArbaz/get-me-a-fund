dff88a9d8fc499d9beca2c6d45290cffbfa1ba9c
AhmedArbaz <ah2797764@gmail.com>
2024-08-07 16:45:20 +0500

    add next portion which add 3 div for payment info

--- app/page.js
+++ app/page.js
@@ -30,14 +30,29 @@ Signup Now
 
   </div>
 
-<div className="text-white">
+<div className="text-white container mx-auto">
 
-  <h1>Your fans can buy you a Chai</h1>
-  <div className="flex gap-5">
-    <div className="item bg-slate-500 rounded-full flex flex-col justify-center items-center p-7">
-      <Image src="/payment.gif" width={88} height={88} alt="hello"/>
-    <p>Fund Yourself</p>
+  <h1 className="text-2xl font-bold text-center my-14">Your fans can buy you a Chai</h1>
+  <div className="flex gap-5 justify-around">
+    <div className="item flex flex-col justify-center items-center">
+      <Image className="bg-slate-500 rounded-full p-2" src="/payment.gif" width={88} height={88} alt="hello"/>
+    <p className="font-bold">Fund Yourself</p>
+    <p>Your fans are available to pay you</p>
     </div>
+
+    <div className="item flex flex-col justify-center items-center">
+      <Image className="bg-slate-500 rounded-full p-2" src="/coin.gif" width={88} height={88} alt="hello"/>
+    <p className="font-bold">Fund Yourself</p>
+    <p>Your fans are available to pay you</p>
+    </div>
+
+    <div className="item flex flex-col justify-center items-center">
+      <Image className="bg-slate-500 rounded-full p-2" src="/groupPayment.gif" width={88} height={88} alt="hello"/>
+    <p className="font-bold">Fans want to help</p>
+    <p>Your fans are available to pay you</p>
+    </div>
+
+    
   </div>
 </div>
     </>
