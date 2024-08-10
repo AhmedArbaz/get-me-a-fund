91fcdb0430c10532fa49bb4cf1f43d70ac112d09
AhmedArbaz <ah2797764@gmail.com>
2024-08-10 16:57:34 +0500

    make condtion that when you logout then redirect to Login page don't stay on dashboard page

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,17 @@
+"use client"
 import React from 'react'
+import { useSession,signIn,signOut } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+//ya same use kar rahy hain redirect karin gay ager banda logout ho jay to dashboard valy page say redirect ho jay 
 
 const dashboard = () => {
+  const {data: session} = useSession()
+  // So ya condition bana di kay ager login nahi ho gay to login page redirect ho jay ga jasay logout karo gay to 
+if (!session) {
+  const router = useRouter()
+  router.push('/Login')
+}
+
   return (
     <div>
       Dashboard
