60c346e5154b591d488e187b04f57fba6ae94d9e
AhmedArbaz <ah2797764@gmail.com>
2024-08-12 15:32:23 +0500

    Add some more entries

--- Components/Dashboard.js
+++ Components/Dashboard.js
@@ -52,6 +52,21 @@ const Dashboard = () => {
             <label HtmlFor="phone" className="block mb-2 text-sm font-medium text-slate-400 dark:text-white">Phone number</label>
             <input type="text" value={form.phone?form.phone:""} onChange={handleChange} id="phone" name='phone' className="bg-slate-700 border border-gray-300 text-slate-400 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
         </div>
+        {/* input for razorpay credientials */}
+        <div>
+            <label HtmlFor="razorpay" className="block mb-2 text-sm font-medium text-slate-400 dark:text-white">Razorpay Credentials</label>
+            <input type="text" value={form.razorpay?form.razorpay:""} onChange={handleChange} id="razorpay" name='razorpay' className="bg-slate-700 border border-gray-300 text-slate-400 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        </div>
+           {/* input for razorpay id */}
+           <div>
+            <label HtmlFor="razorpayid" className="block mb-2 text-sm font-medium text-slate-400 dark:text-white">Razorpay ID</label>
+            <input type="text" value={form.razorpayid?form.razorpayid:""} onChange={handleChange} id="razorpayid" name='razorpayid' className="bg-slate-700 border border-gray-300 text-slate-400 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        </div>
+         {/* input for razorpay id */}
+         <div>
+            <label HtmlFor="razorpaysecret" className="block mb-2 text-sm font-medium text-slate-400 dark:text-white">Razorpay Secret</label>
+            <input type="text" value={form.razorpaysecret?form.razorpaysecret:""} onChange={handleChange} id="razorpaysecret" name='razorpaysecret' className="bg-slate-700 border border-gray-300 text-slate-400 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        </div>
         <div>
             <label HtmlFor="website" className="block mb-2 text-sm font-medium text-slate-400 dark:text-white">Website URL</label>
             <input type="url" value={form.website?form.website:""} onChange={handleChange} id="website" name='website' className="bg-slate-700 border border-gray-300 text-slate-400 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="flowbite.com" required />
